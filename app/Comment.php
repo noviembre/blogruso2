dfb7ae6a9bf337e072bd818eaeaebee463731775
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-12 06:00:05 -0500

    04.1 Creando relaciones
    avi 06. creando mas relaciones y usando el sluggable. GITIGNORE: comenzamos a ignorar todos los archivos cuya extension termine en .directory.

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author()    {
+
+        return $this->belongsTo(User::class);
+    }
+
 }
