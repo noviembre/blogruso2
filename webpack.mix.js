559c3fa60488990fe616f1863eed19cb29739664
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-12 12:39:37 -0500

    06.7 Elixir part 6 Imagenes
    avi08. mostrando las imagenes. crea el dashboard.php se puede apreciar la pagina pero a la hora de cargar las imgenes falla. asi que vamos a webpack.mix.js y agregamos codigo luego en el terminal corremos: npm run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -41,3 +41,5 @@ mix.scripts([
 
 mix.copy('resources/assets/admin/bootstrap/fonts', 'public/fonts');
 mix.copy('resources/assets/admin/dist/fonts', 'public/fonts');
+
+mix.copy('resources/assets/admin/dist/img', 'public/img');
