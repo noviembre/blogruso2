074b96677f9dcc5380af401a77d0da09a92b9cff
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-31 05:37:29 -0500

    25.2 Profile - echo ok
    v23. blog.test/profile al completar los datos del usuario. si todo esta ok, es decir si tu email es un email y tu img es una imagen, etc. te mostrara un ok.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\User;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -12,7 +13,22 @@ class ProfileController extends Controller
     public function index()
     {
         $user = Auth::user();
-        dd($user);
+
         return view('pages.profile', ['user'	=>	$user]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'	=>	'required',
+            'email' =>  [
+                'required',
+                'email',
+                Rule::unique('users')->ignore(Auth::user()->id),
+            ],
+            'avatar'	=>	'nullable|image'
+        ]);
+
+        echo 'ok';
     }
 }
